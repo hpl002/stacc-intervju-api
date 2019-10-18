b8729f3bd24cb08a4fc04ed97956ff222960321d
Herman Platou <hsplatou@gmail.com>
2019-10-18 20:38:13 +0200

    changed to port 5050

--- server.js
+++ server.js
@@ -46,11 +46,8 @@ function handleError(res, reason, message, code) {
     }
   });
 
-/*
-   
- app.listen(5001, () => {
-    console.log("Server is listening on port 5001");
+   app.listen(5050, () => {
+    console.log("Server is listening on port 5050");
 });
- */
-
+ 
   
