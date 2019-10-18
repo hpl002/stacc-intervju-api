90dc224c6638fb8d9f07862470c437ef60b7f29e
Herman Platou <hsplatou@gmail.com>
2019-10-18 19:45:24 +0200

    Initial commit

---
+++ server.js
@@ -0,0 +1,56 @@
+var express = require("express");
+var bodyParser = require("body-parser");
+ 
+ 
+var app = express();
+app.use(bodyParser.json());
+
+// Generic error handler used by all endpoints.
+function handleError(res, reason, message, code) {
+    console.log("ERROR: " + reason);
+    res.status(code || 500).json({"error": message});
+  }
+ 
+  app.get("/api", function(req, res) {
+    db.collection(CONTACTS_COLLECTION).find({}).toArray(function(err, docs) {
+      if (err) {
+        handleError(res, err.message, "Failed to get contacts.");
+      } else {
+        res.status(200).json(docs);
+      }
+    });
+  });
+
+ 
+  
+  app.post("/api", function(req, res) {
+    if (!req.body.laanebelop || req.body.nominellRente || req.body.terminGebyr || req.body.utlopsDato || req.body.saldoDato || req.body.datoForsteInnbetaling || req.body.ukjentVerdi) {
+      handleError(res, "Invalid user input", "All fields must contain data", 400);
+    } else {
+        res.status(200).json({
+            "nedbetalingsplan": {
+              "innbetalinger": [{
+                  "restgjeld": 2000000.0,
+                  "dato": "2020-01-01",
+                  "innbetaling": 0.0,
+                  "gebyr": 0.0,
+                  "renter": 0.0,
+                  "total": 0.0
+                },
+                {
+                  "restgjeld": 1994037.7579585968,
+                  "dato": "2020-01-01",
+                  "innbetaling": 5962.242041403104,
+                  "gebyr": 30.0,
+                  "renter": 0.0,
+                  "total": 5992.242041403104
+                }
+          
+              ]
+            }
+          }
+          );
+    }
+  });
+  
+ 
