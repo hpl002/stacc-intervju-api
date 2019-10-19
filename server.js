686eedbd5efa33eac54e2a3742af06713ac2c930
Herman Platou <hsplatou@gmail.com>
2019-10-19 10:47:25 +0200

    port8000

--- server.js
+++ server.js
@@ -47,11 +47,12 @@ function handleError(res, reason, message, code) {
   });
 
 
-  /*
+  
 
-   app.listen(5050, () => {
-    console.log("Server is listening on port 5050");
+  const port = process.env.PORT || 8000;
+
+   app.listen(port, () => {
+    console.log("Server is listening on port 8000");
 });
-*/
  
   
